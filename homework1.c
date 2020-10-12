a1b307358c4ea0ecddbfda154d286e9c4d3ad0cb
Joker-Mr-Wang <71820790+Joker-Mr-Wang@users.noreply.github.com>
2020-10-13 00:02:12 +0800

    10_13最小公约数
    最小公约数

--- homework1.c
+++ homework1.c
@@ -1,18 +1,14 @@
-#include<stdio.h>
-#include<windows.h>
-#pragma warning(disable:4996)
-int main(){
-	int i = 0;
-	for (i = 1; i <= 100; i++){
-		if ((i % 3) == 0){
-			printf("%d ", i);
-			
-		}
-		else
-		{
-			continue;
-		}
-	}
-	system("pause");
-	return 0;
-}
+//#include<stdio.h>
+//#include<windows.h>
+//int main()
+//{
+//	int i = 0;
+//
+//	for ( i = 1000; i <= 2000; i++){
+//		if ((i % 4 == 0) && (i % 100 != 0) || (i % 400==0)) {
+//			printf("ÈòÄêÎª£º%d\n", i);
+//		}
+//	}
+//	system("pause");
+//	return 0;
+//}
