8e83bb69d6b19b26b07350258b3bf4ff7b4928ae
Joker-Mr-Wang <71820790+Joker-Mr-Wang@users.noreply.github.com>
2020-10-17 14:31:28 +0800

    10_17_100-200的素数
    100-200的素数

---
+++ text.c
@@ -0,0 +1,25 @@
+//#include<stdio.h>
+//#include<windows.h>
+//
+//
+//int main()
+//{
+//	int j = 0;
+//	for (int i = 10; i <= 200; i++){//外层循环控制数字
+//		for ( j = 2; j < i ;j++){//内层控制被除数
+//			if ((i%j) == 0){//余数为零{
+//				break;
+//			}
+//		
+//	     }
+//		if (j == i)  {                  //如果j==i则为素数，并输出
+//			printf("%d\t", i);
+//			
+//			}
+//
+//	}
+//
+//	system("pause");
+//	return 0;
+//
+//}
