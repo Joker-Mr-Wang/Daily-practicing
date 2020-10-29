ad6ec80461f10eee6a4ee8862f711d6708fa4c58
Joker-Mr-Wang <71820790+Joker-Mr-Wang@users.noreply.github.com>
2020-10-29 11:22:21 +0800

    2020_1029_乘法表
    乘法表

---
+++ demo4.c
@@ -0,0 +1,20 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//#include<stdio.h>
+//#include<windows.h>
+//void multiplication(int a){
+//	int i, j;
+//	for (i = 1; i <=a; i++){
+//		for (j = 1; j <= i; j++){
+//			printf("%d*%d=%d\t", i, j, i*j);
+//		}
+//		printf("\n");
+//	}
+//}
+//int main(){
+//	printf("请输入你所需需要的乘法表大小：\n");
+//	int num=0;
+//	scanf("%d", &num);
+//    multiplication(num);
+//	system("pause");
+//		return 0;
+//}
