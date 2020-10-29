45f56c9a69effd5a9d127056e194ccc715fb2946
Joker-Mr-Wang <71820790+Joker-Mr-Wang@users.noreply.github.com>
2020-10-29 11:25:30 +0800

    10_29
    输出数字的每一位数

---
+++ demo5.c
@@ -0,0 +1,20 @@
+//#define _crt_secure_no_warnings
+//#include<stdio.h>
+//#include<windows.h>
+//void print(int num){
+//	if (num < 10){
+//		printf("%d\n", num);
+//	}
+//	else{
+//		print(num / 10);
+//		printf("%d\n", num % 10);
+//	}
+//}
+//int main(){
+//	printf("请输入你所需需要的数字：\n");
+//	int num=0;
+//	scanf("%d", &num);
+//    print(num);
+//	system("pause");
+//		return 0;
+//}
