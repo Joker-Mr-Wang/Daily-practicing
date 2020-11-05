0014b691680ae2502d9be6b30528d58150476855
Joker-Mr-Wang <71820790+Joker-Mr-Wang@users.noreply.github.com>
2020-11-05 23:34:43 +0800

    2020_1105
    统计一个数在二进制中1的个数

--- demo2.c
+++ demo2.c
@@ -1,41 +1,15 @@
-#include<stdio.h>
-#include<windows.h>
-//将数组A中的内容和数组B中的内容进行交换
-void change(int arr1[], int arr2[], int len){
-	for (int i = 0; i < len; i++){
-		int tmp = arr1[i];
-	    arr1[i] = arr2[i];
-	    arr2[i] = tmp;
-	}
-}
-
-int main(){
-	//创建两个数组
-	int arr1[] = { 1, 2, 3, 4 };
-	int arr2[] = { 5, 6, 7, 8 };
-	//求出数组长度
-	int len = sizeof(arr1)/ sizeof(arr1[0]);
-	//翻转前
-	printf("反转前\n ");
-	for (int i = 0; i < len; i++){
-		printf("%d ", arr1[i]);
-	}
-	printf("\n ");
-	for (int i = 0; i < len; i++){
-		printf("%d ", arr2[i]);
-	}
-	printf("\n ");
-	//交换函数
-	change(arr1, arr2, len);
-	//反转后
-	printf("反转后\n ");
-			for (int i = 0; i < len; i++){
-				printf("%d ", arr1[i]);
-			}
-			printf("\n ");
-			for (int i = 0; i < len; i++){
-				printf("%d ", arr2[i]);
-			}
-			system("pause");
-			return 0;
-}
+//#include<stdio.h>
+//#include<windows.h>
+////统计二进制中1的个数
+//int main(){
+//	int num = 11;//所求变量
+//	int count = 0;//定义一个变量用于计算1的个数
+//	for (int i = 0; i < 32; i++){
+//		if (num & (1<< i)){
+//			count++;
+//		}
+//	}
+//	printf("%d在二进制中有%d个1\n", num, count);
+//	system("pause");
+//	return 0;
+//}
