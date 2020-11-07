9614c840e28146fcfc6f4aa9a160ffd359071263
Joker-Mr-Wang <71820790+Joker-Mr-Wang@users.noreply.github.com>
2020-11-07 23:24:45 +0800

    2020_1107逆序输出字符串
    逆序输出字符串

--- demo2.c
+++ demo2.c
@@ -1,15 +1,29 @@
 //#include<stdio.h>
 //#include<windows.h>
-////统计二进制中1的个数
-//int main(){
-//	int num = 11;//所求变量
-//	int count = 0;//定义一个变量用于计算1的个数
-//	for (int i = 0; i < 32; i++){
-//		if (num & (1<< i)){
-//			count++;
+////逆序输出字符串
+//char * StrReversal(char* str){
+//
+//
+//		if (strlen(str) < 2) {
+//			return str;
 //		}
+//		char ch = *str;
+//		int len = strlen(str) - 1;
+//		*str = *(str + len); //把最后一个位置的数据赋值给起始位置
+//		*(str + len) = '\0'; //把当前的最后一个字符数据替换为\0
+//
+//		StrReversal(str + 1);
+//		*(str + len) = ch;
+//		return str;
 //	}
-//	printf("%d在二进制中有%d个1\n", num, count);
+//
+//	
+//	
+//
+//int main(){
+//	char str[] = "abcdefg";
+//	
+//	printf("%s\n", StrReversal(str));
 //	system("pause");
 //	return 0;
 //}
