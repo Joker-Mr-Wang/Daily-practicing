835bc73feeeec1064ff508d4f52e4abf23d5c41a
Joker-Mr-Wang <2977078774@qq.com>
2021-08-05 09:46:44 +0800

    oj

--- OJ/src/main/java/util/DBUtil.java
+++ OJ/src/main/java/util/DBUtil.java
@@ -8,75 +8,74 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-//方便其他的代码随时能够建立数据建立连接~
+// 方便其他的代码随时能和数据库建立连接~
 public class DBUtil {
-    //DataSource 这个东西一般一个程序里面有一个实例就够了~~
-    //单例模式
-    private static final  String URL = "jdbc:mysql://localhost:3306/ojtest?characterEncoding=utf8&useSSL=true";
-    private static final  String USERNAME = "root";
-    private static final  String PASSWORD = "root";
+    // DataSource 这个东西一般一个程序里有一个实例就够了~~
+    // 单例模式
+    private static final String URL = "jdbc:mysql://127.0.0.1:3306/java41_oj?characterEncoding=utf8&useSSL=true";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "2222";
 
     private static DataSource dataSource = null;
 
-    // 目的是只创建出一个DataSource 实例
-    private static DataSource getDataSource(){
-        if (dataSource == null){
-            //没有被实例化就创建一个实例
+    // 目的是只创建出一个 DataSource 实例
+    private static DataSource getDataSource() {
+        if (dataSource == null) {
+            // 没有被实例化过, 就创建一个实例
             dataSource = new MysqlDataSource();
             ((MysqlDataSource)dataSource).setUrl(URL);
             ((MysqlDataSource)dataSource).setUser(USERNAME);
             ((MysqlDataSource)dataSource).setPassword(PASSWORD);
         }
-        //如果已经被实例化过了，就直接返回现有的实例
+        // 如果已经被实例化过了, 就直接返回现有的实例
         return dataSource;
     }
 
-    public static Connection getConnection(){
+    public static Connection getConnection() {
         try {
             return getDataSource().getConnection();
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
         return null;
     }
 
-    //回收资源
-    public static void close(Connection connection, PreparedStatement statement, ResultSet resultSet){
-        if (resultSet != null){
+    // 回收资源
+    public static void close(Connection connection, PreparedStatement statement, ResultSet resultSet) {
+        if (resultSet != null) {
             try {
                 resultSet.close();
             } catch (SQLException e) {
                 e.printStackTrace();
             }
         }
-        if (statement!=null){
+        if (statement != null) {
             try {
                 statement.close();
-            } catch (SQLException throwables) {
-                throwables.printStackTrace();
+            } catch (SQLException e) {
+                e.printStackTrace();
             }
         }
         if (connection != null) {
             try {
                 connection.close();
-            } catch (SQLException throwables) {
-                throwables.printStackTrace();
+            } catch (SQLException e) {
+                e.printStackTrace();
             }
         }
 
 //        try {
-//            if (resultSet!=null){
+//            if (resultSet != null) {
 //                resultSet.close();
 //            }
-//            if (statement!=null){
+//            if (statement != null) {
 //                statement.close();
 //            }
-//            if (connection!=null){
+//            if (connection != null) {
 //                connection.close();
 //            }
 //        } catch (SQLException e) {
 //            e.printStackTrace();
 //        }
-
     }
 }
--- OJ/src/main/java/util/FileUtil.java
+++ OJ/src/main/java/util/FileUtil.java
@@ -1,23 +1,27 @@
 package util;
 
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
-//使用这个类封装一下Java的文件操作。
-//让后面的代码能够更方便的读写整个文件
+// 使用这个类封装一下 Java 的文件操作.
+// 让后面的代码能够更方便的读写整个文件.
 public class FileUtil {
-    //从指定的文件中一次把所有的内容读出来
-    public static String readFile(String filePath){
+    // 从指定的文件中一次把所有的内容读出来
+    public static String readFile(String filePath) {
         StringBuilder stringBuilder = new StringBuilder();
-        try(FileInputStream fileInputStream= new FileInputStream(filePath)) {
-            while (true){
+        try (FileInputStream fileInputStream = new FileInputStream(filePath);) {
+            while (true) {
                 int ch = fileInputStream.read();
-                if (ch==-1){
+                if (ch == -1) {
                     break;
                 }
-                //read方法只是读到一个字节，我们在读出是还需要将int类型转换成byte类型
-              stringBuilder.append((char) ch);
+                // 每次 read 方法只是读到 一个字节 .
+                // read 设计成返回 int 的原因只是为了能多表示一下 -1 这个情况
+                // 实际把读取的结果往 stringBuilder 里插入的时候, 还得再转换回 byte 类型
+                // 预期是一次往 StringBuilder 写一个字符给 StringBuilder, 而不是一次写 四个字节
+                stringBuilder.append((char)ch);
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -25,11 +29,10 @@ public class FileUtil {
         return stringBuilder.toString();
     }
 
-
-    //把content中的内容一次写入到filePath对应的文件中
-    public static void writeFile(String filePath,String content){
-        try(FileOutputStream fileOutputStream = new FileOutputStream(filePath)) {
-            //进行写文件的操作
+    // 把 content 中的内容一次写入到 filePath 对应的文件中
+    public static void writeFile(String filePath, String content) {
+        try (FileOutputStream fileOutputStream = new FileOutputStream(filePath)) {
+            // 进行写文件操作
             fileOutputStream.write(content.getBytes());
         } catch (IOException e) {
             e.printStackTrace();
