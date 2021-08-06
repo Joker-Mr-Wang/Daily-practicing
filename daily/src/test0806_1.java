6df0d736b3801420ec03333d94e90092ac2db27e
Joker-Mr-Wang <2977078774@qq.com>
2021-08-06 22:32:54 +0800

    上传代码

--- daily/src/test0806_1.java
+++ daily/src/test0806_1.java
@@ -1,8 +1,18 @@
+import java.util.Scanner;
+
 public class test0806_1 {
     public static void main(String[] args) {
-        String s = "1";
-        char s1= '1';
-        System.out.println(s.contains(""+s1));
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNext()){
+           String str = scanner.nextLine();
+           StringBuilder stringBuilder = new StringBuilder();
+            for (int i = 0; i <str.length(); i++) {
+                char tmp = str.charAt(i);
+                if (!stringBuilder.toString().contains(tmp+"")){
+                    stringBuilder.append(tmp);
+                }
+            }
+            System.out.println(stringBuilder.toString());
+        }
     }
-
 }
