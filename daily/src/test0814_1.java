5ec27d7d35f5dea2df39154a7f294aeaa5651580
Joker-Mr-Wang <2977078774@qq.com>
2021-08-14 23:34:35 +0800

    提交代码

--- daily/src/test0814_1.java
+++ daily/src/test0814_1.java
@@ -1,5 +1,12 @@
 public class test0814_1 {
     //冒泡排序
+    public static void main(String[] args) {
+        int[] arr = {2,5,7,3,6};
+        Sort(arr);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
         public static void Sort(int[] array) {
             for (int i = 0; i <array.length-1; i++)
                 for (int j = i+1; j<array.length; j++) {
